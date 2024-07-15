59e06867a411cc59beef9912ea10eb84782067a6
marcosvmpinto <58134308+marcosvmpinto@users.noreply.github.com>
2024-07-15 17:28:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
         {
             "title": "Others",
             "revenue": 5015842.37,
-            "image": "https://work.iemfg.com/attachment/ArchiveExtras/122000/122393/line%2012//IMG_4681.JPG",
+            "image": "https://cdn11.bigcommerce.com/s-htnyngf/product_images/uploaded_images/electrical-parts-mb.jpg",
             "link": "https://www.iemfg.com/sites/default/files/2023-12/Custom%20Assemblies_0.pdf",
             "description": "Control, Field Interconnection, Miscellaneous parts, etc."
         }
